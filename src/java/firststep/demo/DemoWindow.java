94fe09db938728420030bdf29db8c259fac39297
Ilya Mizus <bigfatbrowncat@gmail.com>
2015-08-27 11:08:00 +0300

    Firststep updated

--- src/java/firststep/demo/DemoWindow.java
+++ src/java/firststep/demo/DemoWindow.java
@@ -8,6 +8,44 @@ import firststep.Transform;
 import firststep.Window;
 
 public class DemoWindow extends Window {
+
+	private class MainDrawListener implements DrawListener {
+		
+		float angleFunction(float time) {
+			return (float) (-0.3 + 0.21 * Math.atan(time));
+		}
+		
+		float zoomFunction(float time) {
+			return (float) (1 + 0.2 * Math.atan(time - 2));
+		}
+		
+		float blendFunction(float time) {
+			return (float)( 1.0 - Math.pow(Math.max(time - 7.7, 0), 1.5));// Math.min(Math.sqrt(timeSinceStartup / 3), 1.0);
+		}
+		
+		@Override
+		public void draw(Canvas cnv) {
+			float timeSinceStartup = getTimeSinceStartup();
+
+			final float xCenter = DemoWindow.this.getWidth() / 2;
+			final float yCenter = DemoWindow.this.getHeight() / 2;
+			
+			float logoPaintSize = logoController.getLogoFramebufferSize() / 2;
+
+			cnv.setTransform(
+					Transform.rotating(angleFunction(timeSinceStartup))
+					.scale(zoomFunction(timeSinceStartup), 
+					
+					zoomFunction(timeSinceStartup)
+			).translate(xCenter, yCenter));
+
+			Paint logoFbPaint = cnv.imagePattern(- logoPaintSize / 2, - logoPaintSize / 2, logoPaintSize, logoPaintSize, 0, logoController.getLogoFramebuffer().getImage(), blendFunction(timeSinceStartup));
+			cnv.beginPath();
+			cnv.fillPaint(logoFbPaint);
+			cnv.rect(- logoPaintSize / 2, - logoPaintSize / 2, logoPaintSize, logoPaintSize);
+			cnv.fill();
+		}
+	};
 	
 	private static final String APPNAME = "Welcome to firststep";
 	
@@ -18,12 +56,14 @@ public class DemoWindow extends Window {
 
 	private static float backRed = 0.15f, backGreen = 0.15f, backBlue = 0.1f;
 
+	private MainDrawListener mainDrawListener = new MainDrawListener();
+
 	private float getTimeSinceStartup() {
 		return (float)((double)System.currentTimeMillis() - startupMoment) / 1000;
 	}
 	
 	@Override
-	protected void frame() {
+	protected void beforeFrame() {
 		logoController.setCurrentTime(getTimeSinceStartup());	// ???
 	}
 	
@@ -32,41 +72,7 @@ public class DemoWindow extends Window {
 		
 		float foreRed = 0.8f, foreGreen = 0.8f, foreBlue = 0.7f;
 		logoController = new LogoController(this, foreRed, foreGreen, foreBlue);
-		
-		getMainFramebuffer().setDrawListener(new DrawListener() {
-			
-			float angleFunction(float time) {
-				return (float) (-0.3 + 0.21 * Math.atan(time));
-			}
-			
-			float zoomFunction(float time) {
-				return (float) (1 + 0.2 * Math.atan(time - 2));
-			}
-			
-			float blendFunction(float time) {
-				return (float)( 1.0 - Math.pow(Math.max(time - 7.7, 0), 1.5));// Math.min(Math.sqrt(timeSinceStartup / 3), 1.0);
-			}
-			
-			@Override
-			public void draw(Canvas cnv) {
-				float timeSinceStartup = getTimeSinceStartup();
-
-				final float xCenter = width / 2;
-				final float yCenter = height / 2;
-				
-				float logoPaintSize = logoController.getLogoFramebufferSize() / 2;
-
-				cnv.setTransform(Transform.rotating(angleFunction(timeSinceStartup)).scale(zoomFunction(timeSinceStartup), zoomFunction(timeSinceStartup)).translate(xCenter, yCenter));
-
-				Paint logoFbPaint = cnv.imagePattern(- logoPaintSize / 2, - logoPaintSize / 2, logoPaintSize, logoPaintSize, 0, logoController.getLogoFramebuffer().getImage(), blendFunction(timeSinceStartup));
-				cnv.beginPath();
-				cnv.fillPaint(logoFbPaint);
-				cnv.rect(- logoPaintSize / 2, - logoPaintSize / 2, logoPaintSize, logoPaintSize);
-				cnv.fill();
-			}
-		});
-
-
+		getMainFramebuffer().setDrawListener(mainDrawListener);
 	}
 	
 	public DemoWindow() {
